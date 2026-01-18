fbb9383e801f08cb65baa1d9d23d10288c638b01
itssloplayz <155429915+majcek210@users.noreply.github.com>
2026-01-18 14:26:08 +0100

    New workflow for trust publishing

--- tests/main.ts
+++ tests/main.ts
@@ -10,7 +10,11 @@ client.registerCommandsRoute("./dist/tests/Commands")
 client.discord.on("messageCreate", (msg:any) => {
     if (msg.content === "!ping") msg.reply("pong")
 })
-
+client.discord.once("ready", () => {
+    setTimeout(() => {
+        process.exit(0)
+    }, 20000)
+})
 
 client.start(process.env.TOKEN)
 client.pushCommands(process.env.TOKEN)
